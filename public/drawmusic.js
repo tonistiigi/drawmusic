b831e6d3e90da867701d8f67fdcc080feaf5d8fb
Tõnis Tiigi <tonistiigi@gmail.com>
2013-03-22 22:57:07 +0200

    Some basic function skeleton

---
+++ public/drawmusic.js
@@ -0,0 +1,39 @@
+// I know these global fuctions are wrong. I know!
+
+
+function setupLayers(points, endImage) {
+
+}
+
+function layerComplete() {
+
+}
+
+function layerProgress() {
+
+}
+
+
+function startGame(level) {
+
+}
+
+function endGame() {
+
+}
+
+
+var level = {
+  name: 'twinkle',
+  layers: [
+    { tiles: [
+      {x: 100, y: 120, src: 'tile001.png'},
+      {x: 100, y: 120, src: 'tile002.png'}
+      ],
+      src: 'img.png',
+      audio: 'audio.mp3'
+    }
+  ],
+  duration: 12.3
+}
+
