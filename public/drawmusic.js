68c19f8f28433fddf15795d72e106e35a4811eb3
Indrek <works@indrek.eu>
2013-03-23 10:45:40 +0200

    ready to provide layer data with images

--- public/drawmusic.js
+++ public/drawmusic.js
@@ -1,8 +1,15 @@
 // I know these global fuctions are wrong. I know!
 
+function preloadResources() {
+    preloadImages(level);
+}
 
 function setupLayers(points, endImage) {
     // NOT MY CODE TO WRITE, remove as you want (Indrek)
+    console.debug('Setting up new layer with first point as x: %s y: %s with endImage: %s',
+        points[0].x, points[0].y, endImage);
+        
+    // When we're done
     layerComplete();
 }
 
@@ -10,7 +17,9 @@ function layerComplete() {
     console.debug('Level %s complete.', currentLayer);
     if (currentLayer < level.layers.length - 1) {
         currentLayer++;
-        setupLayers(null, null); // järg
+        setupLayers(level.layers[currentLayer].tiles, level.layers[currentLayer].src);
+    } else {
+        endGame(); // We only have one level currently
     }
 }
 
@@ -23,12 +32,26 @@ function startGame() {
     alert('Careful, we are now starting!');
     console.debug('Total levels: %s', level.layers.length);
     currentLayer = 0;
-    setupLayers(level.layers[currentLayer], null);
-    
+    setupLayers(level.layers[currentLayer].tiles, null);
 }
 
 function endGame() {
+    console.debug('Game ended');
     alert('Well done. Would you like to buy our "Bird is the Word" version?');
+}
+
+function preloadImages(level) {
+    level.layers.forEach(function(layer) {
+    
+        layer.tiles.forEach(function(tile) {
+            if (tile.src != null) {
+                var img = new Image();
+                img.src = tile.src;
+                console.debug('Successfully preloaded image %s', img.src);
+            }
+        });
+        
+    });
 }
 
 var currentLayer = -1;
@@ -37,18 +60,18 @@ var level = {
   name: 'twinkle',
   layers: [
     { tiles: [
-      {x: 100, y: 120, src: ''},
-      {x: 100, y: 150, src: 'tile001.png'},
-	  {x: 130, y: 140, src: 'tile003.png'}
+      {x: 100, y: 100, src: null},
+      {x: 100, y: 200, src: 'twinkle_l1t1.png'},
+	  {x: 200, y: 200, src: 'twinkle_l1t2.png'}
       ],
       src: 'img.png',
       audio: 'audio.mp3'
     },
 	
 	{ tiles: [
-      {x: 200, y: 150, src: ''},
-      {x: 200, y: 200, src: 'tile001.png'},
-	  {x: 300, y: 300, src: 'tile003.png'}
+      {x: 200, y: 200, src: null},
+      {x: 200, y: 100, src: 'twinkle_l2t1.png'},
+	  {x: 100, y: 100, src: 'twinkle_l2t2.png'}
       ],
       src: 'img.png',
       audio: 'audio.mp3'
