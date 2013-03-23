a406d49f5e13857b5f7a08eaf641706f523e3b34
Mikk Laos <mikk.laos@gmail.com>
2013-03-23 20:57:34 +0200

    bug fixed

--- public/connectDots.js
+++ public/connectDots.js
@@ -8,19 +8,37 @@ var radius = 20;
 var nextPoint
 var isdown
 var points
+var currentPoint
+var correctSelection
 
 $(document).ready(function() {
-
-  $('#canvas').mousedown(function() {
-    isdown = true
+	
+  $('#canvas').mousedown(function(e) {
+	isdown = true;
   })
   $('#canvas').mouseup(function() {
-    isdown = false
+   
+	isdown = false;
+	correctSelection=false
+	
+
+
   })
 
 	$("#canvas").mousemove(function(e) {
     // console.log(e)
-    if (isdown && !lcomplete && isNextPoint(e.offsetX, e.offsetY)) {
+
+    var parentOffset = $(this).offset(); 
+	var relX = e.pageX - parentOffset.left;
+    var relY = e.pageY - parentOffset.top;
+	  if (!correctSelection && isdown && currentPoint){
+
+    correctSelection = isCurrentPoint(relX, relY);
+	
+
+	}
+
+    if (correctSelection && !lcomplete && isNextPoint(relX, relY)) {
 
       var outline = $('<div class="outline"></div>')
 
@@ -42,6 +60,7 @@ $(document).ready(function() {
 function setupLayer(p, src) {
   console.log('setup')
   points = p
+  currentPoint = points[0]
   for (var i = 0 ;i < p.length; i++) {
 		var xx = p[i].x
 		var yy = p[i].y
@@ -54,10 +73,10 @@ function setupLayer(p, src) {
 var pointNum = 0;
 function drawPoint(top, left) {
 	var t = $('<div>')
-	t.css("top", top);
-	t.css("left", left)
+	t.css("top", top-10);
+	t.css("left", left-10)
 	t.addClass("point")
-	t.html(pointNum);
+	t.text(pointNum+1);
 	pointNum++;
 	$("#canvas").append(t);
 }
@@ -74,15 +93,25 @@ function isNextPoint(x, y) {
 				layerComplete();
 				console.log("picture is complete");
 			}
+			currentPoint=points[pointNumber];
 			pointNumber++;
 			return true;
 		} else {
 			startPointSet = true;
 			nextPoint = points[1];
-			pointNumber = 1;
+			pointNumber=1;
+			currentPoint=points[0];
 		}
 	}
 	return false;
+}
+
+function isCurrentPoint(x,y){
+	if (getDistance(currentPoint, {x : x, y : y}) < radius) {
+		return true
+	
+	}
+return false
 }
 
 function getLayerProgress() {
